6e8659f3bd6c0c01070eadce6d71c55fee4d07b0
Rafael Giordano Vieira <rafaelgiordano12@gmail.com>
2022-02-19 13:30:00 -0300

    Corrigindo arquivo antecessor

--- antecessor.py
+++ antecessor.py
@@ -1,2 +1,4 @@
-x = int(input("Digite um número: "))
-print("Sucessor = {}, Antecessor = {}.".format(x+1, x-1))
+# Capturing input data and casting it to integer
+number = int(input("Informe um número: "))
+# Printing the number sucessor and antecessor
+print("Sucessor = {}, Antecessor = {}.".format(number + 1, number - 1))
