c50fab61a70c7c2a54a93735a4c06f66c1e1a2d3
Jean Lago <100036946+jeanlago@users.noreply.github.com>
2022-02-19 13:50:40 -0300

    pequenas correções.

---
+++ dobro.py
@@ -0,0 +1,7 @@
+'''esse codigo tem como objetivo calcular o dobro, triplo e raizq'''
+
+numero = int(input("Digite um número: "))
+dobro = numero * 2
+triplo = numero * 3
+raizq = numero** 2
+print(f"Dobro = {dobro}, triplo = {triplo}, raiz quadrada = {raizq}")
