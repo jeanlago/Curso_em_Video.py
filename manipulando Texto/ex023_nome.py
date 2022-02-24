21b003ea64e9d95a40062a5b393cd3b6ff5944ca
Jean Lago <100036946+jeanlago@users.noreply.github.com>
2022-02-24 17:19:35 -0300

    + Exercícios.

---
+++ manipulando Texto/ex023_nome.py
@@ -0,0 +1,9 @@
+'''Exercício 23'''
+
+NAME = str(input("Digite seu nome: "))
+print(NAME.upper())
+print(NAME.lower())
+print(len(NAME.replace(" ", "")))
+NAME = (NAME.split())
+#print(len(NAME))
+print(len(NAME[0]))
